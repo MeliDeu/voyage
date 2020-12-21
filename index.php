aea90005f7cdae2d603438bb2513a2fc95d7dd49
KAJSA OJALA <kajsaojala@hotmail.com>
2020-12-21 17:28:41 +0100

    lagt in en root med färger och fonter i css, samt länkat font i index

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&family=Source+Sans+Pro:wght@300;400;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
     <form id="login" action="admin/login.php" method="POST">
