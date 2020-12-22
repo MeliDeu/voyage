69e6660bafdeb78b5f9091e5d4da82a8186e4c55
KAJSA OJALA <kajsaojala@hotmail.com>
2020-12-22 11:17:48 +0100

    början på startsidan

--- index.php
+++ index.php
@@ -4,36 +4,57 @@
     include "sections/header.php";
 
 ?>
+    <div id="bgStart"></div>
+    <div id="wrapper">
 
+        <!--nav på startsida-->
+        <nav class="flexCenter startNav">
+            <div id="logo">V</div>
+            <div id="links">
+                <a href="/">About</a>
+                <button class="button">Login/Join</button>
+            </div>
+        </nav>
 
-    <form id="login" action="admin/login.php" method="POST">
-        <h2 class="formTitle">Log in</h2>
+        <!--titel på startsidan-->
+        <div class="startTitle flexCenter">
+            <h1>Collect your <span>travels</span> to inspire and be <span>inspired</span></h1>
+        </div>
 
-        <?php if (isset($_GET["error"])) { ?>
-        <p class="error">Oh no, invalid username or password, please try again!</p>
-        <?php } ?>
-                    
-        <p class="formSmallTitle">Username</p>
-        <input type="text" name="username" placeholder="Enter your username"><br>
-        <p class="formSmallTitle">Password</p>
-        <input type="password" name="password" placeholder="Enter your password"><br>
-        <button type="submit">Log in</button>
-    </form>
-    <form id="register" action="home.php" method="POST">
-        <h2 class="formTitle">Sign up</h2>
-        <p id="errorRegister"></p>
+        <!--LOGGA IN-->
+        <form id="login" action="admin/login.php" method="POST">
+            <h2 class="formTitle">Log in</h2>
+
+            <?php if (isset($_GET["error"])) { ?>
+            <p class="error">Oh no, invalid username or password, please try again!</p>
+            <?php } ?>
                         
-        <p class="formSmallTitle">Username</p>
-        <input id="newUsername" type="text" name="username" placeholder="New username"><br>
-        <p class="formSmallTitle">Email</p>
-        <input id="newEmail" type="text" name="email" placeholder="Email address">
-        <p class="formSmallTitle">Password</p>
-        <input id="newPassword" type="password" name="password" placeholder="New password"><br>
-        <p class="forSmallTitle">Intreseted in new travel friends</p>
-        <p>Yes</p>
-        <input id="travelStatus" type="checkbox" name="travelStatus">
-        <button class="register" type="submit">Register</button>
-    </form>
+            <p class="formSmallTitle">Username</p>
+            <input type="text" name="username" placeholder="Enter your username"><br>
+            <p class="formSmallTitle">Password</p>
+            <input type="password" name="password" placeholder="Enter your password"><br>
+            <button type="submit">Log in</button>
+        </form>
+
+        <!--REGISTRERA-->
+        <form id="register" action="home.php" method="POST">
+            <h2 class="formTitle">Sign up</h2>
+            <p id="errorRegister"></p>
+                            
+            <p class="formSmallTitle">Username</p>
+            <input id="newUsername" type="text" name="username" placeholder="New username"><br>
+            <p class="formSmallTitle">Email</p>
+            <input id="newEmail" type="text" name="email" placeholder="Email address">
+            <p class="formSmallTitle">Password</p>
+            <input id="newPassword" type="password" name="password" placeholder="New password"><br>
+            <p class="forSmallTitle">Intreseted in new travel friends</p>
+            <p>Yes</p>
+            <input id="travelStatus" type="checkbox" name="travelStatus">
+            <button class="register" type="submit">Register</button>
+        </form>
+    
+    
+    </div>
 
 <?php
     include "sections/footer.php";
