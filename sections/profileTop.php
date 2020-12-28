d7055b0882a6504731738371491893d215e7fb4a
Jasmine Jasse <jasminejasses@gmaill.com>
2020-12-28 22:52:19 +0100

    börjat på style för profileTop

---
+++ sections/profileTop.php
@@ -0,0 +1,39 @@
+<div id='profileContainer'>
+
+<div id='profileBox'>
+
+        <div id='profilePic'></div>
+        <div id='profileInfo'>
+            <div id='profileName'>Mandy1</div>
+            <div id='profileBio'>Hejsan hoppsan här kommer en kort presentation om vem jag är typ</div>
+            <div id='profileInterest'>
+                <div id='interestIcon'></div>
+                <div id='interestText'>$name is interested in new travel friends</div>
+            </div>
+        </div>
+    </div>
+
+    <div id='profileFavs'>
+
+        <div id='topFavs'>
+            <span>Top favs</span>
+            <ul>
+                <li>Indonesia</li>
+                <li>Australia</li>
+                <li>Scotland</li>
+            </ul>
+        </div>
+
+        <div id='topWants'>
+            <span>Top wants</span>
+                <ul>
+                    <li>Canada</li>
+                    <li>Iceland</li>
+                    <li>Italy</li>
+                </ul>
+        </div>
+    </div>
+
+    <div id='profileSettings'></div>
+</div>
+
