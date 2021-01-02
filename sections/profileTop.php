e94d2c9804491f07dbd9f87fef2760790a04176d
KAJSA OJALA <kajsaojala@hotmail.com>
2021-01-02 16:00:45 +0100

    rätt username syns beroende på vem man klickar på

--- sections/profileTop.php
+++ sections/profileTop.php
@@ -1,21 +1,14 @@
 <?php
-    $username = $_SESSION["username"];
+    $database = getDatabase(); //hämtar databasen
 
-    $file = "admin/db.json";  //detta gör vi typ likadant i alla filer just nu så borde skapa en funktion som vi kallar på som hämtar databasen
-    $database = [];
-    if (file_exists($file)) {
-        $data = file_get_contents($file);
-        $database = json_decode($data, true);
-    }
-    $loggedInId = false;
     $bio = false;
     $travelInterest = false;
     $top3Wishes = false;
     $top3Favs = false;
+
     foreach($database['users'] as $index => $user){
-        if($user['id'] == $clickedUserId){
-            // Id:t kan vi nu använda för att få fram den personens content
-            $loggedInId = $user['id'];
+        if($user['id'] == $clickedUserId){ //$clickedUserId får sitt värde i home.php
+            $username = $user['username'];
             $bio = $user['bio'];
             $travelInterest = $user['travelStatus'];
             $top3Wishes = $user['top3Wishes'];
@@ -27,23 +20,19 @@
 
 <div id='profileBox'>
 
-        <div id='profilePic'></div>
-        <div id='profileInfo'>
-            <div id='profileName'><?php echo $username; ?></div>
-            <div id='profileBio'><?php echo $bio; ?></div>
-            <textarea name="patchBio" id="patchBio" class="hide" cols="50" rows="4"></textarea>
+    <div id='profilePic'></div>
+    <div id='profileInfo'>
+        <div id='profileName'><?php echo $username; ?></div>
+        <div id='profileBio'><?php echo $bio; ?></div>
+        <textarea name="patchBio" id="patchBio" class="hide" cols="50" rows="4"></textarea>
+
+        <?php if ($travelInterest !== false){ ?>
             <div id='profileInterest'>
                 <div id='interestIcon'></div>
-                <div id='interestText'><?php echo $username;
-                    if ($travelInterest !== false){
-                        echo " is interested in new travel friends";
-                    }
-                    else{
-                        echo "is not interested in new travel friends";
-                    }
-                 ?></div>
+                <div id='interestText'><?php echo "$username is interested in new travel friends";?></div>
             </div>
-        </div>
+        <?php } ?>
+    </div>
     </div>
 
     <div id='profileFavs'>
