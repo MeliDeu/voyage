3ce49431b37a98154675e473d028aa46ba9423e4
MeliDeu <m.deuretzbacher@yahoo.se>
2021-01-09 00:19:54 +0100

    changed request url

--- js/uploadScript.js
+++ js/uploadScript.js
@@ -45,7 +45,7 @@ function newPostToDB() {
     });
     //om alla fält är ifyllda, ska begäran skickas
     if (newPost.validate()) {
-        let nyRequ = new Request("/admin/uploadPost.php", {
+        let nyRequ = new Request("../admin/uploadPost.php", {
             method: "POST",
             body: formData
         });
